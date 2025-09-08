712ccee85e573fd91ff1a8d93390918e1097bdd1
Luís Oliveira <luis_oliveira15@hotmail.com>
2025-09-08 17:23:02 +0100

    Alterações no package.json
    Alteração para o Render fazer deploy da raiz do repositório. E não apenas da pasta backend

--- backend/server.js
+++ backend/server.js
@@ -60,6 +60,13 @@ app.use("/api/reports", reportRoutes);
 // Servir arquivo upload
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
+//  Servir o frontend compilado
+app.use(express.static(path.join(__dirname, "dist"))); 
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
 // Iniciar o servidor
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
